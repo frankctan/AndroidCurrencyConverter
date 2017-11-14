244b31bd4090004722b79617f1619495bb87159b
Frank Tan <frankctan@gmail.com>
2017-11-13 22:52:21 -0500

    Delete convert button and add textwatcher

--- app/src/main/java/com/example/firstapptaketwo/MainActivity.java
+++ app/src/main/java/com/example/firstapptaketwo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.firstapptaketwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,30 +26,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, TextWatcher {
 
-
-
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private String value;
-    private String currency;
-
-    String url = "http://api.fixer.io/latest?base=USD";
-    String data = "";
+    // What are the currency conversion rates?
     Map<String, Double> rates = new HashMap<>();
 
+    // What is the selected currency?
+    String selected = "USD";
+
+    // What are the currencies displayed?
     ArrayList<Currency> currencyArray = new ArrayList<>();
 
-
-    RequestQueue requestQueue;
+    // What is the ListView's dataSource?
     ArrayAdapter<Currency> arrayAdapter;
+
+    // Networking
+    String url = "http://api.fixer.io/latest?base=USD";
+    RequestQueue requestQueue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // boilerplate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configure text changed listener.
+        EditText editText = findViewById(R.id.editText);
+        editText.addTextChangedListener(this);
+
+        // Configure currency spinner.
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.currencies_array, android.R.layout.simple_spinner_item);
@@ -55,6 +63,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        // Rates are based on USD.
+        rates.put(getString(R.string.usd), 1.0);
+
+        // Add default values for the listView dataSource.
         Currency usd = new Currency(getString(R.string.usd), 1.0);
         Currency gbp = new Currency(getString(R.string.gbp), 1.0);
         Currency eur = new Currency(getString(R.string.eur), 1.0);
@@ -65,14 +77,17 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         currencyArray.add(eur);
         currencyArray.add(nzd);
 
-        arrayAdapter = new ArrayAdapter<Currency>(this, android.R.layout.simple_list_item_2, android.R.id.text1, currencyArray) {
+        // Customize arrayAdapter to provide special values for textfields.
+        arrayAdapter = new ArrayAdapter<Currency>(this, android.R.layout.simple_list_item_2,
+                android.R.id.text1, currencyArray) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
                 TextView text1 = view.findViewById(android.R.id.text1);
                 TextView text2 = view.findViewById(android.R.id.text2);
 
-                text1.setText(currencyArray.get(position).value.toString());
+                Double val = currencyArray.get(position).value;
+                text1.setText(val.toString());
                 text2.setText(currencyArray.get(position).name);
                 return view;
             }
@@ -84,8 +99,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         getData();
     }
-
-
 
     protected void getData() {
         requestQueue = Volley.newRequestQueue(this);
@@ -100,6 +113,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                             String eurKey = getString(R.string.eur);
                             String nzdKey = getString(R.string.nzd);
 
+                            // Update rates based on API call.
                             Double gbp = obj.getDouble(gbpKey);
                             Double eur = obj.getDouble(eurKey);
                             Double nzd = obj.getDouble(nzdKey);
@@ -108,28 +122,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                             rates.put(eurKey, eur);
                             rates.put(nzdKey, nzd);
 
-                            for (int i = 0; i < currencyArray.size(); i++) {
-                                Currency _c = currencyArray.get(i);
-                                Double r = rates.get(_c.name);
-                                if (r == null) {
-                                    continue;
-                                }
-                                Currency c = new Currency(_c.name, r);
-                                currencyArray.set(i, c);
-                            }
-
-                            arrayAdapter.notifyDataSetInvalidated();
-
+                            updateListView();
                         }
-                        // Try and catch are included to handle any errors due to JSON
+                        // Handle errors produced by JSON call.
                         catch (JSONException e) {
-                            // If an error occurs, this prints the error to the log
                             e.printStackTrace();
                         }
                     }
                 },
-                // The final parameter overrides the method onErrorResponse() and passes VolleyError
-                //as a parameter
                 new Response.ErrorListener() {
                     @Override
                     // Handles errors that occur due to Volley
@@ -142,18 +142,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         requestQueue.add(obreq);
     }
 
+    public void updateListView() {
+        // Get user input from editText.
+        EditText editText = findViewById(R.id.editText);
+        String text = editText.getText().toString();
+
+        Double input = 1.0;
+        if (!text.equals("")) {
+            input = Double.parseDouble(text);
+        }
+
+        // Update the list view based on the selected currency and input values.
+        for(int i = 0; i < currencyArray.size(); i++) {
+            Currency _c = currencyArray.get(i);
+            Double conversion = rates.get(_c.name) / rates.get(selected);
+            Currency c = new Currency(_c.name, input * conversion);
+            currencyArray.set(i, c);
+        }
+
+        arrayAdapter.notifyDataSetInvalidated();
+    }
+
+    // AdapterView.OnItemSelectedListener Implementation
+
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        String item = (String) parent.getItemAtPosition(pos);
-        this.currency = item;
-        Log.d("DEBUG::", this.currency);
+        String selectedCurrency = (String) parent.getItemAtPosition(pos);
+        selected = selectedCurrency;
+        updateListView();
     }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+    public void onNothingSelected(AdapterView<?> parent) {}
+
+    // TextWatcher Implementation
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        updateListView();
     }
 
-    public void convertDidTap(View v) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        this.value = editText.getText().toString();
-    }
+    @Override
+    public void afterTextChanged(Editable editable) {}
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 }
